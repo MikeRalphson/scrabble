263cc01daa3d027adfde8cbae74f09776c992b13
unknown <mike.ralphson@gmail.com>
2016-02-24 05:09:16 +0000

    Cope with blanks (omitted letters)

--- scrabble.js
+++ scrabble.js
@@ -8,17 +8,35 @@ Array.prototype.remove = function(from, to) {
   return this.push.apply(this, rest);
 };
 
+var input = [];
+var lookup = [];
+var seen = [];
+
+function addLetters(wordLookup,original,limit,display) {
+	for (var l=0;l<26;l++) {
+		var addLetter = String.fromCharCode('a'.charCodeAt(0)+l);
+		var newLookup = (wordLookup+addLetter).split('').sort().join('');
+		if (newLookup.length<limit) {
+			addLetters(newLookup,original,limit,addLetter + ' + ' + display);
+		}
+		for (var i=0;i<input.length;i++) {
+			if ((lookup[i] == newLookup) && (seen.indexOf(input[i]) < 0)) {
+				seen.push(input[i]);
+				console.log(original + ' + ' + display + addLetter + ' -> ' + input[i]);
+			}
+		}
+	}
+}
+
 if (process.argv.length>2) {
-	
+
 	var word = process.argv[2];
 
 	var infile = './dict/words8';
 	console.log('Processing '+infile);
-	var input = [];
 	input = fs.readFileSync(infile,'utf8').split('\n');
 	console.log('Words: '+input.length);
-	
-	var lookup = [];
+
 	var temp = [];
 	for (var i=0;i<input.length;i++) {
 		temp = input[i].split(',');
@@ -27,27 +45,20 @@ if (process.argv.length>2) {
 	}
 
 	var wordLookup = word.split('').sort().join('');
-	
+
 	//console.log(word + ' ' + wordLookup);
-	
+
 	for (var i=0;i<input.length;i++) {
 		if (lookup[i] == wordLookup) {
 			console.log(wordLookup + ' -> ' + input[i]);
 		}
 	}
 
-	for (var l=0;l<26;l++) {
-		var addLetter = String.fromCharCode('a'.charCodeAt(0)+l);
-		var newLookup = (wordLookup+addLetter).split('').sort().join('');
-		//console.log('Checking addition of ' + addLetter + ' ' + newLookup);
-		for (var i=0;i<input.length;i++) {
-			if (lookup[i] == newLookup) {
-				console.log(wordLookup + ' + ' + addLetter + ' -> ' + input[i]);
-			}
-		}
-		
+	for (var i=wordLookup.length;i<9;i++) {
+		addLetters(wordLookup,wordLookup,i,'');
 	}
+
 }
 else {
-	console.log('Usage: scrabble word');
+	console.log('Usage: scrabble {letters}');
 }
